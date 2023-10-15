08da65f81de83b77f258f81986f9692c31bb866f
Pablo Gancedo Jiménez <gancedo@MacBook-Pro-de-Pablo.local>
2023-10-15 23:19:47 +0200

    funcionabilidad js a la calculadora

---
+++ script.js
@@ -0,0 +1,114 @@
+let currentDisplay = "0";
+let currentOperator = "";
+let firstOperand = null;
+let waitingForSecondOperand = false;
+let resultCalculated = false;
+
+function updateDisplay() {
+  const display = document.getElementById("display");
+  display.textContent = currentDisplay.replace(".", ",");
+}
+
+function appendNumber(number) {
+  if (resultCalculated) {
+    // Si se ha calculado un resultado anteriormente, reinicia las operaciones.
+    clearDisplay();
+  }
+
+  if (currentDisplay === "0" || waitingForSecondOperand) {
+    currentDisplay = number.toString();
+    waitingForSecondOperand = false;
+    resultCalculated = false;
+  } else if (currentDisplay.length < 9) {
+    currentDisplay += number;
+  }
+  updateDisplay();
+}
+
+function appendDecimal() {
+  if (waitingForSecondOperand) return;
+  if (!currentDisplay.includes(",")) {
+    currentDisplay += ",";
+  }
+  updateDisplay();
+}
+
+function appendOperator(operator) {
+  if (waitingForSecondOperand) {
+    currentOperator = operator;
+    return;
+  }
+  if (firstOperand === null) {
+    firstOperand = parseFloat(currentDisplay.replace(",", "."));
+  } else if (currentOperator) {
+    const result = performCalculation();
+    currentDisplay = formatResult(result);
+    firstOperand = result;
+  }
+  currentOperator = operator;
+  waitingForSecondOperand = true;
+  resultCalculated = false;
+  updateDisplay();
+}
+
+function performCalculation() {
+  const operand1 = parseFloat(firstOperand);
+  const operand2 = parseFloat(currentDisplay.replace(",", "."));
+  if (currentOperator === "+") {
+    return operand1 + operand2;
+  } else if (currentOperator === "-") {
+    return operand1 - operand2;
+  } else if (currentOperator === "*") {
+    return operand1 * operand2;
+  } else if (currentOperator === "/") {
+    return operand1 / operand2;
+  }
+}
+
+function formatResult(result) {
+  // Limita el resultado a 9 caracteres y redondea el último dígito.
+  const roundedResult = parseFloat(result.toFixed(8));
+  return roundedResult.toString().replace(".", ",").substring(0, 9);
+}
+
+function calculateResult() {
+  if (currentOperator && !waitingForSecondOperand) {
+    const result = performCalculation();
+    currentDisplay = formatResult(result);
+    firstOperand = result;
+    currentOperator = "";
+    waitingForSecondOperand = false;
+    resultCalculated = true;
+    updateDisplay();
+  }
+}
+
+function clearDisplay() {
+  currentDisplay = "0";
+  currentOperator = "";
+  firstOperand = null;
+  waitingForSecondOperand = false;
+  resultCalculated = false;
+  updateDisplay();
+}
+
+function toggleSign() {
+  if (currentDisplay !== "0") {
+    currentDisplay = (parseFloat(currentDisplay.replace(",", ".")) * -1)
+      .toString()
+      .replace(".", ",");
+    updateDisplay();
+  }
+}
+
+function percentage() {
+  currentDisplay = (parseFloat(currentDisplay.replace(",", ".")) / 100)
+    .toString()
+    .replace(".", ",");
+  updateDisplay();
+}
+
+// Agrega más funciones y características según sea necesario.
+
+// Llama a updateDisplay() inicialmente para mostrar "0" en la pantalla.
+updateDisplay();
